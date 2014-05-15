42254252b088f930917519b3a8764981cdbdf846
spotco <mootothemax@gmail.com>
2014-05-14 23:33:18 -0700

    beta test fix

--- top.js
+++ top.js
@@ -10,8 +10,11 @@ function rescale() {
 
     $("#logo_sign").css({"margin-top":(150+scf*200)+"px"})
     $("#sign_top").css({width:scf*220+"px", height:scf*100+"px", top: scf*-75 + "px"})
-    $("#sign_top > img").css({width:scf*220+"px", height:scf*100+"px"})
+    $("#sign_top_pane").css({width:scf*220+"px", height:scf*100+"px"})
+
+    //app store logo
     $("#sign_top > a > img").css({width:scf*200+"px",height:scf*59+"px"})
+    
     $("#sign_top > a").css({bottom:scf*85+"px", margin:(6+scf*4)+"px"})
     $("#main_logo").css({top:-(150+scf*200)+"px", width: Math.min(100,(78 + (1-scf)*60))+"%"})
     $("#vid").css({top:(40*scf)+"px", "padding-left":((1-scf)*30)+"px"})
